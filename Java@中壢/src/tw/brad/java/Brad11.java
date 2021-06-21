8a84721a95839ebc513580d9da6c92a8282e65d9
brad <brad@brad.tw>
2021-06-21 15:50:42 +0800

    06211550

--- Java@中壢/src/tw/brad/java/Brad11.java
+++ Java@中壢/src/tw/brad/java/Brad11.java
@@ -4,12 +4,20 @@ public class Brad11 {
 
 	public static void main(String[] args) {
 		
-		// for, loop
-		for (int i=0; i< 10 ; i++) { 
-			System.out.println("Hello, World" + i);
+		int i = 0;
+		for (m1() ; i < 10 ; m2()) { 
+			System.out.println("Hello, World" + i++);
 		}
-		System.out.println("---");
+		//System.out.println("---");
+		//System.out.println(i);
 		
+	}
+	
+	static void m1() {
+		System.out.println("Brad");
+	}
+	static void m2() {
+		System.out.println("-----");
 	}
 
 }
