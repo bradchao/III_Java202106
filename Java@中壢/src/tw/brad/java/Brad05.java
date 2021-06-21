8b20a5be29c4a3b9950109bcb13c97691c30bed1
brad <brad@brad.tw>
2021-06-21 14:05:14 +0800

    06211405

---
+++ Java@中壢/src/tw/brad/java/Brad05.java
@@ -0,0 +1,18 @@
+package tw.brad.java;
+
+public class Brad05 {
+
+	public static void main(String[] args) {
+		// float, double
+		float var1 = 123;
+		float var2 = 12.3f;
+		
+		double var3 = 123;
+		double var4 = 12.3;	
+		
+		double a = 0,  b = 0;
+		System.out.println(a / b);
+		
+	}
+
+}
---
+++ Java@中壢/src/tw/brad/java/Brad06.java
@@ -0,0 +1,23 @@
+package tw.brad.java;
+
+public class Brad06 {
+
+	public static void main(String[] args) {
+		// 2^16 => 0 ~ 65535
+		char c1 = 65;
+		System.out.println(c1);
+		char c2 = '$';
+		char c3 = '資';
+		System.out.println(c3);
+		
+		boolean isOK = false;
+		System.out.println(isOK);
+		isOK = !isOK;
+		System.out.println(isOK);
+		// byte, short, int, long
+		// float, double
+		// char
+		// boolean
+	}
+
+}
