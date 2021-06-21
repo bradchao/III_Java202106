baf6ca56412db93d1eab0106bffaf866b4613299
brad <brad@brad.tw>
2021-06-21 14:55:12 +0800

    06211456

---
+++ Java@中壢/src/tw/brad/java/Brad08.java
@@ -0,0 +1,14 @@
+package tw.brad.java;
+
+public class Brad08 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		if (a > 10) {
+			System.out.println("OK");
+			System.out.println("OK2");
+		}
+		
+	}
+
+}
