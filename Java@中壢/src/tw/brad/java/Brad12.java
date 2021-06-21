e1cad59617c960290aac3d27d5792dc2b0fd8522
brad <brad@brad.tw>
2021-06-21 16:28:18 +0800

    06211627

--- Java@中壢/src/tw/brad/java/Brad12.java
+++ Java@中壢/src/tw/brad/java/Brad12.java
@@ -3,11 +3,14 @@ package tw.brad.java;
 public class Brad12 {
 
 	public static void main(String[] args) {
+		int row = 5;
+		int col = 3;
+		int start = 0;
 		
-		for (int k=0; k<2; k++) {
+		for (int k=0; k<row; k++) {
 			for (int j=1; j<=9; j++) {
-				for (int i=2; i<=5; i++) {
-					int newi = i + k * 4;
+				for (int i=start; i< start + col; i++) {
+					int newi = i + k * col;
 					int r = newi * j;
 					System.out.print(String.format("%d x %d = %d\t", newi, j, r));
 				}
