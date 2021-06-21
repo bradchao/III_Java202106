6a5adc838a79c4364c89ea740b069da623c532bd
brad <brad@brad.tw>
2021-06-21 11:54:48 +0800

    06211154

---
+++ Java@中壢/src/tw/brad/java/Brad02.java
@@ -0,0 +1,11 @@
+package tw.brad.java;
+
+public class Brad02 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 3;
+		System.out.println(a / b); 	// byte, short, int => 
+		System.out.println(a % b);
+	}
+
+}
---
+++ Java@中壢/src/tw/brad/java/Brad03.java
@@ -0,0 +1,18 @@
+package tw.brad.java;
+
+import java.util.Scanner;
+
+public class Brad03 {
+
+	public static void main(String[] args) {
+		int a, b;
+		
+		Scanner scanner = new Scanner(System.in);
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		int c = a + b;
+		System.out.println(c);
+		
+	}
+
+}
