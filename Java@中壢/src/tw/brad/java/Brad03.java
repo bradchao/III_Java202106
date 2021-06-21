d14cc2dc31988a18dd06c4d546b0b6930ff18664
brad <brad@brad.tw>
2021-06-21 13:41:29 +0800

    06211341

--- Java@中壢/src/tw/brad/java/Brad03.java
+++ Java@中壢/src/tw/brad/java/Brad03.java
@@ -15,8 +15,16 @@ public class Brad03 {
 		System.out.print("b = ");
 		b = scanner.nextInt();
 		
-		int c = a + b;
-		System.out.println(String.format("%d + %d = %d", a, b, c));
+		int v1 = a + b;
+		int v2 = a - b;
+		int v3 = a * b;
+		int v4 = a / b;
+		int v5 = a % b;
+		
+		System.out.println(String.format("%d + %d = %d", a, b, v1));
+		System.out.println(String.format("%d - %d = %d", a, b, v2));
+		System.out.println(String.format("%d x %d = %d", a, b, v3));
+		System.out.println(String.format("%d / %d = %d ...... %d", a, b, v4, v5));
 		
 	}
 
