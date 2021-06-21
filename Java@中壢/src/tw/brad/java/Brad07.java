d28f0ac944f3c5352d8a5ac8576644ad5b715be2
brad <brad@brad.tw>
2021-06-21 14:47:50 +0800

    06211447

--- Java@中壢/src/tw/brad/java/Brad07.java
+++ Java@中壢/src/tw/brad/java/Brad07.java
@@ -10,16 +10,27 @@ public class Brad07 {
 //		Scanner scanner = new Scanner(System.in);
 //		System.out.print("Score = ");
 //		score = scanner.nextInt();
-		
 
 		// 亂數產生 0 - 100 的整數成績值 101個值
 		score = (int)(Math.random()*101);
 		System.out.println(score);
 		
-		if (score >= 60) {
-			System.out.println("Pass");
+		if (score >= 90) {
+			System.out.println("A");
 		}else {
-			System.out.println("Down");
+			if (score >=80) {
+				System.out.println("B");
+			}else {
+				if (score >=70) {
+					System.out.println("C");
+				}else {
+					if (score >=60) {
+						System.out.println("D");
+					}else {
+						System.out.println("E");
+					}
+				}
+			}
 		}
 		
 		System.out.println("---");
