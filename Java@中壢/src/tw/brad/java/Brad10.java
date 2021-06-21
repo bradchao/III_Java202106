adc8a8c4c2fa26f768c61d6c9856ae204ec088bb
brad <brad@brad.tw>
2021-06-21 15:20:10 +0800

    06211520

--- Java@中壢/src/tw/brad/java/Brad10.java
+++ Java@中壢/src/tw/brad/java/Brad10.java
@@ -8,10 +8,19 @@ public class Brad10 {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Month = ");
 		int month = scanner.nextInt();
-		int days;
+		int days = 0;
 		
-		
-		
+		switch (month) {
+			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+				days = 31;
+				break;
+			case 4: case 6: case 9: case 11:
+				days = 30;
+				break;
+			case 2:
+				days = 28;
+				break;
+		}
 		
 		System.out.println(String.format("%d月有%d天", month, days));
 		
