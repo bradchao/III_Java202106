543da95e2549b6c9ed441a8022599f8304fbb20a
brad <brad@brad.tw>
2021-06-21 10:45:54 +0800

    06211045

---
+++ Java@中壢/src/tw/brad/java/HelloWorld.java
@@ -0,0 +1,17 @@
+package tw.brad.java;
+
+// 英文不用好, 只要不怕英文, 就可以
+// 紅色 => Java 關鍵字 => 全小寫 => 60 個以內 => 應用
+// public => 存取 => 全世界 
+// Brad => brad@brad.tw
+public class HelloWorld {
+
+	// 執行進入點
+	public static void main(String[] brad) {
+		// 印出 Hello, World
+		// 在執行作業系統(Windows/Mac/Linux跨平台)下的標準輸出印出 Hello, World
+		System.out.println("Hello, World");
+		
+	}
+	
+}
