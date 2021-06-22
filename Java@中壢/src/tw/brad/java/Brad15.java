8715d485237759866a59f3f8900583c99e1d168d
brad <brad@brad.tw>
2021-06-22 10:05:47 +0800

    06221005

--- Java@中壢/src/tw/brad/java/Brad15.java
+++ Java@中壢/src/tw/brad/java/Brad15.java
@@ -5,10 +5,10 @@ public class Brad15 {
 	public static void main(String[] args) {
 		int[] p = new int[7];	// p[0]=0, ... p[6]=0
 		
-		for (int i=0; i<100; i++) {
-			int point = (int)(Math.random()*6)+1;
-			if (point>=1 && point <= 6) {
-				p[point]++;
+		for (int i=0; i<10000000; i++) {
+			int point = (int)(Math.random()*9)+1;
+			if (point>=1 && point <= 9) {
+				p[ point>6 ? point-3 : point ]++;
 			}else{
 				p[0]++;
 			}
