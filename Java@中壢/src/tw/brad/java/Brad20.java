8e97ab3decd622512f93046e24b7dbf544579887
brad <brad@brad.tw>
2021-06-22 16:31:59 +0800

    06221631

--- Java@中壢/src/tw/brad/java/Brad20.java
+++ Java@中壢/src/tw/brad/java/Brad20.java
@@ -1,12 +1,26 @@
 package tw.brad.java;
 
 import tw.brad.myclasses.Bike;
+import tw.brad.myclasses.Scooter;
 
 public class Brad20 {
 
 	public static void main(String[] args) {
 		Bike myBike = new Bike();
-		System.out.println("OK");
+		Bike urBike = new Bike();
+		System.out.println("myBike:" + myBike.getSpeed());
+		System.out.println("urBike:" + urBike.getSpeed());
+		
+		for (int i=0; i<10; i++) myBike.upSpeed();
+		System.out.println("myBike:" + myBike.getSpeed());
+		System.out.println("---");
+		Scooter s1 = new Scooter();
+		s1.upSpeed();
+		s1.upSpeed();s1.upSpeed();s1.upSpeed();
+		s1.downSpeed();
+		System.out.println(s1.getSpeed());
+		
+		
 	}
 
 }
