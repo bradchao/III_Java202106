46810b585899a4097dfb02a26b0bca4ec6ff88ea
brad <brad@brad.tw>
2021-06-22 11:55:00 +0800

    06221154

---
+++ Java@中壢/src/tw/brad/java/PokerV1.java
@@ -0,0 +1,34 @@
+package tw.brad.java;
+
+public class PokerV1 {
+
+	public static void main(String[] args) {
+		// 洗牌
+		int[] poker = new int[52];
+		for (int i=0; i<poker.length; i++) {
+			int rand = (int)(Math.random()*52);	// 0 - 51
+			
+			// 檢查機制
+			boolean isRepeat = false;
+			for (int j=0; j<i; j++) {
+				if (poker[j] == rand) {
+					// 重複了
+					isRepeat = true;
+					break;
+				}
+			}
+			
+			if (!isRepeat) {
+				poker[i] = rand;
+				System.out.println(poker[i]);
+			}else {
+				i--;
+			}
+			
+		}
+				
+		// 發牌
+		// 攤牌 -> 理牌
+	}
+
+}
