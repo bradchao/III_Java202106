4e72218572f40c154ae53977081713a1b443f58e
brad <brad@brad.tw>
2021-06-22 15:35:37 +0800

    06221535

---
+++ Java@中壢/src/tw/brad/java/Brad20.java
@@ -0,0 +1,12 @@
+package tw.brad.java;
+
+import tw.brad.myclasses.Bike;
+
+public class Brad20 {
+
+	public static void main(String[] args) {
+		Bike myBike = new Bike();
+		System.out.println("OK");
+	}
+
+}
