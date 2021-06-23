6ffb4ce90ff36874a230797fbebd76fae828b0e4
brad <brad@brad.tw>
2021-06-23 10:21:10 +0800

    06231021

---
+++ Java@中壢/src/tw/brad/java/Brad22.java
@@ -0,0 +1,22 @@
+package tw.brad.java;
+
+import javax.swing.JFrame;
+
+public class Brad22 {
+
+	public static void main(String[] args) {
+		JFrame w1 = new JFrame();
+		w1.setTitle("I am W1");
+		w1.setSize(640, 480);
+		w1.setVisible(true);
+		w1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JFrame w2 = new JFrame();
+		w2.setTitle("I am W2");
+		w2.setSize(640, 480);
+		w2.setVisible(true);
+		w2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
