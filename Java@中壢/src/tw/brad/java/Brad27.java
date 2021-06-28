8d3b0b0cbc9bdc20d0203bbc0e27e01b776b2c3c
brad <brad@brad.tw>
2021-06-28 09:33:34 +0800

    06280933

--- Java@中壢/src/tw/brad/java/Brad27.java
+++ Java@中壢/src/tw/brad/java/Brad27.java
@@ -5,7 +5,7 @@ import tw.brad.myclasses.TWId;
 public class Brad27 {
 
 	public static void main(String[] args) {
-		if (TWId.isRightTWId("A223456789")) {
+		if (TWId.isRightTWId("A123456789")) {
 			System.out.println("OK");
 		}else {
 			System.out.println("XX");
