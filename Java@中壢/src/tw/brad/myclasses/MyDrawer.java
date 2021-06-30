3dcdd1300459e4492a5ebfca28961801006bdc1a
brad <brad@brad.tw>
2021-06-30 15:41:58 +0800

    06301541

--- Java@中壢/src/tw/brad/myclasses/MyDrawer.java
+++ Java@中壢/src/tw/brad/myclasses/MyDrawer.java
@@ -10,16 +10,18 @@ import java.util.LinkedList;
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel {
-	private LinkedList<HashMap<String,Integer>> line;
+	private LinkedList<LinkedList<Point>> lines;
 	
 	public MyDrawer() {
 		setBackground(Color.GREEN);
 		
-		line = new LinkedList<>();
+		lines = new LinkedList<>();
 		
 		MyMouseAdapter adapter = new MyMouseAdapter();
 		addMouseMotionListener(adapter);
 	}
+
+	// 想請問paintComponent什麼時候會被呼叫
 
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -27,25 +29,43 @@ public class MyDrawer extends JPanel {
 		
 		g.setColor(Color.BLUE);
 		
-		for(int i=1; i<line.size(); i++) {
-			HashMap<String, Integer> p0 = line.get(i-1);
-			HashMap<String, Integer> p1 = line.get(i);
-			g.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"));
+		for (LinkedList<Point> line : lines) {
+			for(int i=1; i<line.size(); i++) {
+				Point p0 = line.get(i-1);
+				Point p1 = line.get(i);
+				g.drawLine(p0.x, p0.y, p1.x, p1.y);
+			}
 		}
 		
 	}
 	
 	private class MyMouseAdapter extends MouseAdapter {
 		@Override
-		public void mouseDragged(MouseEvent e) {
-			HashMap<String,Integer> point = new HashMap<>();
-			// x -> 12; y -> 34
-			point.put("x", e.getX()); point.put("y", e.getY());
+		public void mousePressed(MouseEvent e) {
+			LinkedList<Point> line = new LinkedList<>();
+			Point point = new Point(e.getX(), e.getY()); 
 			line.add(point);
+			lines.add(line);
+			repaint();
+		}
+		
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			
+//			HashMap<String,Integer> point = new HashMap<>();
+//			// x -> 12; y -> 34
+//			point.put("x", e.getX()); point.put("y", e.getY());
+			
+			Point point = new Point(e.getX(), e.getY()); 
+			lines.getLast().add(point);
 			repaint();
 		}
 	}
-	
+}
+
+class Point {
+	int x, y;
+	Point(int x, int y){this.x = x ; this.y = y;}
 }
 
 
